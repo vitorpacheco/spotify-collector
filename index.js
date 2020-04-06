cc39fdb51d3e92abcb6a80581a3ddcda6b56c46c
Vitor Pacheco <vpacheco.costa@gmail.com>
2020-04-06 00:16:42 -0300

    Project creation

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+
+const Telegraf = require('telegraf')
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+
+bot.telegram.getMe().then((botInfo) => {
+    console.log(botInfo)
+    bot.options.username = botInfo.username
+})
+
+bot.start((ctx) => ctx.reply('Wellcome!'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.launch()
